51f674d6ef54b6d9b00e827d89694ef40c469590
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-10-26 09:19:50 +0200

    fixed the fix

--- src/i18n.js
+++ src/i18n.js
@@ -3,8 +3,8 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const apiKey = `${process.env.REACT_APP_I18_API_KEY}`;
-const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
+const API_KEY = `${process.env.REACT_APP_I18_API_KEY}`;
+const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${API_KEY}`;
 
 i18next
   .use(HttpBackend)
