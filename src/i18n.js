f467de0f2fedfe2479706dcdf06c3b8f50cd4eca
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-10-26 09:11:10 +0200

    fix/ key

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const apiKey = "l2FvITpSc9x3BoPkJJJfrw";
+const apiKey = `${process.env.REACT_APP_I18_API_KEY}`;
 const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
 
 i18next
